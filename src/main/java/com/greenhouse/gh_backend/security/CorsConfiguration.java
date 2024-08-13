948a11dd57d058d6186e9c842440afa6d0904ff2
Rania_Wachene <125664751+RaniaWachene1@users.noreply.github.com>
2024-08-13 09:28:51 +0200

    Updated deployment

--- src/main/java/com/greenhouse/gh_backend/security/CorsConfiguration.java
+++ src/main/java/com/greenhouse/gh_backend/security/CorsConfiguration.java
@@ -11,8 +11,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("/")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
